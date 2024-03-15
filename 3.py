d9449b7f2065e42bf0720afa91a02d53d4fdfbc1
Yujith01 <163491629+Yujith01@users.noreply.github.com>
2024-03-15 09:00:15 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,68 @@
+def aStarAlgo(start_node, stop_node):
+    open_set = {start_node}
+    closed_set = set()
+    g = {start_node: 0}
+    parents = {start_node: start_node}
+
+    while len(open_set) > 0:
+        n = None
+        for v in open_set:
+            if n is None or g[v] + heuristic(v) < g[n] + heuristic(n):
+                n = v
+
+        if n is None:
+            print('Path does not exist!')
+            return None
+
+        if n == stop_node:
+            path = []
+            while parents[n] != n:
+                path.append(n)
+                n = parents[n]
+            path.append(start_node)
+            path.reverse()
+            print('Path found:', path)
+            return path
+
+        open_set.remove(n)
+        closed_set.add(n)
+
+        for (m, weight) in get_neighbors(n):
+            if m in closed_set:
+                continue
+            if m not in open_set:
+                open_set.add(m)
+            if g.get(m, float('inf')) > g[n] + weight:
+                g[m] = g[n] + weight
+                parents[m] = n
+
+    print('Path does not exist!')
+    return None
+
+def get_neighbors(v):
+    if v in Graph_nodes:
+        return Graph_nodes[v]
+    else:
+        return None
+
+def heuristic(n):
+    H_dist = {
+        'A': 11,
+        'B': 6,
+        'C': 99,
+        'D': 1,
+        'E': 7,
+        'G': 0,
+    }
+    return H_dist[n]
+
+Graph_nodes = {
+    'A': [('B', 2), ('E', 3)],
+    'B': [('C', 1), ('G', 9)],
+    'C': None,
+    'E': [('D', 6)],
+    'D': [('G', 1)],
+}
+
+aStarAlgo('A', 'G')
+
